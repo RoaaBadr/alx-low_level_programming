d3ee7123c8986a6d0c65ec6aeb9821e17d058939
myusername <myemail@gmail.com>
2023-10-21 17:14:55 +0300

    ubdated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- 	* struct dog - a dog's basic info
- 	* @name: First member
- 	* @age: Second member
- 	* @owner: Third member
- 	* Description: Longer description
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
  */
 struct dog
 {
@@ -13,11 +13,9 @@ struct dog
 	float age;
 	char *owner;
 };
-
-
 /**
-	* dog_t - typedef for struct dog
-*/
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -26,3 +24,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 #endif
+
