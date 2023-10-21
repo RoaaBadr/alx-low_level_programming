b4a0e22be0fa9f854c1feeef1c45a2db67903d46
myusername <myemail@gmail.com>
2023-10-21 17:01:36 +0300

    ubdated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,14 +25,14 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-		r++;
-	}
-	if (str[r] == '\0')	
-	{
-		str[r++] = '\n';
-	}
-	return (str);
+		{
+			str[r] = av[i][n];
+			r++;
+		}
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
+		return (str);
 	}
 }
